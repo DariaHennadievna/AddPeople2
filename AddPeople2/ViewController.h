96393323dda185c61512925f81f565bd6fa7e2cd
Admin <admin@Mac-Admin.local>
2015-04-19 15:23:22 +0300

    Initial Commit

---
+++ AddPeople2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AddPeople2
+//
+//  Created by Admin on 19.04.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
